f7d8f93aba8d5a6ba4cc8ccb8f99ce7aac4d9661
Ranbir Singh <rsingh@ventanamicro.com>
2025-10-13 23:35:55 +0530

    lib: Add MM service group create and destroy support
    Co-authored-by: Sunil V L <sunilvl@ventanamicro.com>
    Signed-off-by: Ranbir Singh <rsingh@ventanamicro.com>

---
+++ lib/rpmi_service_group_mm.c
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2025 Ventana Micro Systems Inc.
+ */
+
+#include <librpmi.h>
+
+#ifdef DEBUG
+#define PREFIX_STR  "== LIBRPMI: MM: =========> %s: %03u: "
+
+#define DPRINTF(msg...)							\
+	{								\
+		rpmi_env_printf(PREFIX_STR, __func__, __LINE__);	\
+		rpmi_env_printf(msg);					\
+		rpmi_env_printf("\n");					\
+	}
+#else
+#define DPRINTF(msg...)
+#endif
+
+#define RPMI_MM_MAJOR_VER   (0x1UL)
+#define RPMI_MM_MINOR_VER   0x0
+
+#define MM_MAJOR_VER_MASK   0xEFFF0000
+#define MM_MINOR_VER_MASK   0x0000FFFF
+#define MM_MAJOR_VER_SHIFT  16
+
+struct rpmi_mm_attr {
+	rpmi_uint32_t mm_version;
+	rpmi_uint32_t shmem_addr_lo;
+	rpmi_uint32_t shmem_addr_hi;
+	rpmi_uint32_t shmem_size;
+};
+
+struct rpmi_service_group_mm {
+	struct rpmi_mm_attr mma;
+	struct rpmi_service_group group;
+};
+
+static enum rpmi_error rpmi_mm_get_attributes(struct rpmi_service_group *group,
+					      struct rpmi_service *service,
+					      struct rpmi_transport *xport,
+					      rpmi_uint16_t request_datalen,
+					      const rpmi_uint8_t *request_data,
+					      rpmi_uint16_t *response_datalen,
+					      rpmi_uint8_t *response_data)
+{
+	return RPMI_ERR_NOTSUPP;
+}
+
+static enum rpmi_error rpmi_mm_communicate(struct rpmi_service_group *group,
+					   struct rpmi_service *service,
+					   struct rpmi_transport *xport,
+					   rpmi_uint16_t request_datalen,
+					   const rpmi_uint8_t *request_data,
+					   rpmi_uint16_t *response_datalen,
+					   rpmi_uint8_t *response_data)
+{
+	return RPMI_ERR_NOTSUPP;
+}
+
+/* Keep entry index same as service_id value */
+static struct rpmi_service rpmi_mm_services[RPMI_MM_SRV_ID_MAX] = {
+	[1] = {
+	       .service_id = RPMI_MM_SRV_ENABLE_NOTIFICATION,
+	       .min_a2p_request_datalen = 0,
+	       .process_a2p_request = NULL,
+	        },
+	[2] = {
+	       .service_id = RPMI_MM_SRV_GET_ATTRIBUTES,
+	       .min_a2p_request_datalen = 0,
+	       .process_a2p_request = rpmi_mm_get_attributes,
+	        },
+	[3] = {
+	       .service_id = RPMI_MM_SRV_COMMUNICATE,
+	       .min_a2p_request_datalen = 4,
+	       .process_a2p_request = rpmi_mm_communicate,
+	        },
+};
+
+struct rpmi_service_group
+*rpmi_service_group_mm_create(rpmi_uint32_t shmem_addr_hi,
+			      rpmi_uint32_t shmem_addr_lo,
+			      rpmi_uint32_t shmem_size)
+{
+	struct rpmi_service_group_mm *sgmm;
+	struct rpmi_service_group *group;
+
+	/* Allocate MM service group */
+	sgmm = rpmi_env_zalloc(sizeof(*sgmm));
+	if (!sgmm) {
+		DPRINTF("failed to allocate MM service group instance");
+		return NULL;
+	}
+
+	sgmm->mma.mm_version =
+	    ((RPMI_MM_MAJOR_VER << MM_MAJOR_VER_SHIFT) & MM_MAJOR_VER_MASK) |
+	    ((RPMI_MM_MINOR_VER & MM_MINOR_VER_MASK));
+	sgmm->mma.shmem_addr_hi = shmem_addr_hi;
+	sgmm->mma.shmem_addr_lo = shmem_addr_lo;
+	sgmm->mma.shmem_size = shmem_size;
+
+	group = &sgmm->group;
+	group->name = "mm";
+	group->servicegroup_id = RPMI_SRVGRP_MANAGEMENT_MODE;
+	group->servicegroup_version =
+	    RPMI_BASE_VERSION(RPMI_SPEC_VERSION_MAJOR, RPMI_SPEC_VERSION_MINOR);
+	/* Allowed only for M-mode RPMI context */
+	group->privilege_level_bitmap = RPMI_PRIVILEGE_M_MODE_MASK;
+	group->max_service_id = RPMI_MM_SRV_ID_MAX;
+	group->services = rpmi_mm_services;
+	group->process_events = NULL;
+	group->lock = rpmi_env_alloc_lock();
+	group->priv = sgmm;
+
+	return group;
+}
+
+void rpmi_service_group_mm_destroy(struct rpmi_service_group *group)
+{
+	if (!group) {
+		DPRINTF("invalid parameters");
+		return;
+	}
+
+	rpmi_env_free(group->priv);
+}
