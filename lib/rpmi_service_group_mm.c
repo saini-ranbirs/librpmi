322bef85df9f10c1f939966d3911c83641422b32
Ranbir Singh <rsingh@ventanamicro.com>
2025-10-15 10:38:52 +0530

    lib: Add MM_VAR_FN_GET_PAYLOAD_SIZE support
    Co-authored-by: Sunil V L <sunilvl@ventanamicro.com>
    Signed-off-by: Ranbir Singh <rsingh@ventanamicro.com>

--- lib/rpmi_service_group_mm.c
+++ lib/rpmi_service_group_mm.c
@@ -4,6 +4,8 @@
  */
 
 #include <librpmi.h>
+#include "librpmi_internal.h"
+#include "rpmi_mm.h"
 
 #ifdef DEBUG
 #define PREFIX_STR  "== LIBRPMI: MM: =========> %s: %03u: "
@@ -37,6 +39,70 @@ struct rpmi_service_group_mm {
 	struct rpmi_service_group group;
 };
 
+struct rpmi_mm_comm_req {
+	rpmi_uint32_t idata_off;
+	rpmi_uint32_t idata_len;
+	rpmi_uint32_t odata_off;
+	rpmi_uint32_t odata_len;
+};
+
+struct rpmi_mm_comm_header_guid {
+	enum efi_mm_header_guid name;
+	struct efi_guid guid;
+};
+
+struct rpmi_mm_comm_header_guid mm_comm_hdr_guid_lut[] = {
+	[0] { EFI_MM_HDR_GUID_NONE, EFI_MM_HDR_GUID_NONE_DATA },
+	[1] { EFI_MM_VAR_PROTOCOL_GUID, EFI_MM_VAR_PROTOCOL_GUID_DATA },
+};
+
+#define MAX_TRANSFER_SIZE  (1 * 1024)	/* 1 KB */
+static rpmi_uint8_t payload_buffer[MAX_PAYLOAD_SIZE];
+rpmi_uint8_t msg_buffer[MAX_TRANSFER_SIZE];
+
+#ifdef DEBUG
+
+static rpmi_uint16_t mm_calls_counter = 0;
+
+#define STRING_CASE(x)  case x: return #x
+
+static const char *get_hdr_guid_string(enum efi_mm_header_guid guid)
+{
+	switch (guid) {
+		STRING_CASE(EFI_MM_VAR_PROTOCOL_GUID);
+
+	default:
+		STRING_CASE(EFI_MM_HDR_GUID_UNSUPPORTED);
+	}
+}
+
+static const char *null_string = "NULL";
+
+static const char *get_var_fn_string(rpmi_uint32_t function_code)
+{
+	switch (function_code) {
+		STRING_CASE(MM_VAR_FN_GET_VARIABLE);
+		STRING_CASE(MM_VAR_FN_GET_NEXT_VARIABLE_NAME);
+		STRING_CASE(MM_VAR_FN_SET_VARIABLE);
+		STRING_CASE(MM_VAR_FN_QUERY_VARIABLE_INFO);
+		STRING_CASE(MM_VAR_FN_READY_TO_BOOT);
+		STRING_CASE(MM_VAR_FN_EXIT_BOOT_SERVICE);
+		STRING_CASE(MM_VAR_FN_GET_STATISTICS);
+		STRING_CASE(MM_VAR_FN_LOCK_VARIABLE);
+		STRING_CASE(MM_VAR_FN_VAR_CHECK_VARIABLE_PROPERTY_SET);
+		STRING_CASE(MM_VAR_FN_VAR_CHECK_VARIABLE_PROPERTY_GET);
+		STRING_CASE(MM_VAR_FN_GET_PAYLOAD_SIZE);
+		STRING_CASE(MM_VAR_FN_INIT_RUNTIME_VARIABLE_CACHE_CONTEXT);
+		STRING_CASE(MM_VAR_FN_SYNC_RUNTIME_CACHE);
+		STRING_CASE(MM_VAR_FN_GET_RUNTIME_CACHE_INFO);
+
+	default:
+		return null_string;
+	}
+}
+
+#endif /* DEBUG */
+
 static enum rpmi_error rpmi_mm_get_attributes(struct rpmi_service_group *group,
 					      struct rpmi_service *service,
 					      struct rpmi_transport *xport,
@@ -65,6 +131,84 @@ static enum rpmi_error rpmi_mm_get_attributes(struct rpmi_service_group *group,
 	return RPMI_SUCCESS;
 }
 
+static inline int get_guid_index(const rpmi_uint8_t *guid,
+				 rpmi_uint16_t msg_len)
+{
+	rpmi_uint8_t i;
+
+	for (i = 1; i < array_size(mm_comm_hdr_guid_lut); i++) {
+		if (rpmi_env_memcmp((void *)guid, &mm_comm_hdr_guid_lut[i].guid,
+				    msg_len))
+			continue;
+
+		return i;
+	}
+
+	return 0;
+}
+
+static inline rpmi_uint64_t fn_get_payload_size(rpmi_uint8_t *comm_hdr_data,
+						rpmi_uint32_t payload_size)
+{
+	struct mm_var_comm_get_payload_size *get_payload_size;
+
+	if (payload_size < sizeof(struct mm_var_comm_get_payload_size))
+		return EFI_INVALID_PARAMETER;
+
+	get_payload_size = (struct mm_var_comm_get_payload_size *)comm_hdr_data;
+	get_payload_size->var_payload_size = MAX_PAYLOAD_SIZE;
+
+	return EFI_SUCCESS;
+}
+
+static enum rpmi_error mm_var_fn_handler(void *comm_buf, rpmi_uint64_t bufsize)
+{
+	rpmi_uint64_t status = EFI_SUCCESS, payload_size;
+	struct mm_var_comm_header *var_comm_hdr;
+
+	if (comm_buf == NULL) {
+		DPRINTF("Nothing to do.");
+		return RPMI_SUCCESS;
+	}
+
+	if (bufsize < MM_VAR_COMM_HEADER_SIZE) {
+		DPRINTF("MM comm buffer size invalid !!!");
+		return RPMI_SUCCESS;
+	}
+
+	payload_size = bufsize - MM_VAR_COMM_HEADER_SIZE;
+	DPRINTF("bufsize = %ld hdrsize = %ld payload_size = %ld",
+		bufsize, MM_VAR_COMM_HEADER_SIZE, payload_size);
+
+	if (payload_size > MAX_PAYLOAD_SIZE) {
+		DPRINTF("MM comm buffer payload size invalid > %ld !!!",
+			MAX_PAYLOAD_SIZE);
+		return RPMI_SUCCESS;
+	}
+
+	rpmi_env_memset(payload_buffer, 0x00, MAX_PAYLOAD_SIZE);
+	var_comm_hdr = (struct mm_var_comm_header *)comm_buf;
+
+	switch (var_comm_hdr->function) {
+	case MM_VAR_FN_GET_PAYLOAD_SIZE:
+		DPRINTF("Processing %s mm_calls_counter %u",
+			get_var_fn_string(var_comm_hdr->function),
+			++mm_calls_counter);
+		status = fn_get_payload_size(var_comm_hdr->data, payload_size);
+		break;
+
+	default:
+		status = EFI_UNSUPPORTED;
+		DPRINTF("%s not supported",
+			get_var_fn_string(var_comm_hdr->function));
+		break;
+	}
+
+	var_comm_hdr->return_status = status;
+
+	return RPMI_SUCCESS;
+}
+
 static enum rpmi_error rpmi_mm_communicate(struct rpmi_service_group *group,
 					   struct rpmi_service *service,
 					   struct rpmi_transport *xport,
@@ -73,7 +217,59 @@ static enum rpmi_error rpmi_mm_communicate(struct rpmi_service_group *group,
 					   rpmi_uint16_t *response_datalen,
 					   rpmi_uint8_t *response_data)
 {
-	return RPMI_ERR_NOTSUPP;
+	struct rpmi_service_group_mm *sgmm = group->priv;
+	struct efi_mm_comm_header *mm_comm_hdr, *msg;
+	rpmi_uint32_t *rsp = (void *)response_data;
+	rpmi_uint64_t status = RPMI_ERR_NO_DATA;
+	struct rpmi_mm_comm_req *mmc_req;
+	rpmi_uint64_t msg_len, mm_addr;
+	rpmi_uint8_t index;
+
+	if (!request_data)
+		return RPMI_ERR_NO_DATA;
+
+	mmc_req = (struct rpmi_mm_comm_req *)request_data;
+	mm_addr = sgmm->mma.shmem_addr_hi;
+	mm_addr = (mm_addr << 32) | sgmm->mma.shmem_addr_lo;
+	mm_addr = mm_addr + mmc_req->idata_off;
+
+	rpmi_env_readb(mm_addr, (rpmi_uint8_t *)&msg_buffer,
+		       sizeof(struct efi_mm_comm_header));
+
+	mm_comm_hdr = (struct efi_mm_comm_header *)msg_buffer;
+	msg_len =
+	    offsetof(struct efi_mm_comm_header, data) + mm_comm_hdr->msg_len;
+
+	msg = (struct efi_mm_comm_header *)msg_buffer;
+	rpmi_env_readb(mm_addr, (rpmi_uint8_t *)msg, msg_len);
+
+	index = get_guid_index((rpmi_uint8_t *)&msg->hdr_guid,
+			       sizeof(msg->hdr_guid));
+
+	switch (mm_comm_hdr_guid_lut[index].name) {
+	case EFI_MM_VAR_PROTOCOL_GUID:
+		DPRINTF("Handling header %s",
+			get_hdr_guid_string(mm_comm_hdr_guid_lut[index].name));
+		status = mm_var_fn_handler(&msg->data, msg_len);
+		rpmi_env_writeb(mm_addr + mmc_req->odata_off,
+				(rpmi_uint8_t *)msg, msg_len);
+		break;
+
+	default:
+		DPRINTF("Header guid %s",
+			get_hdr_guid_string(mm_comm_hdr_guid_lut[index].name));
+		status = RPMI_ERR_NO_DATA;
+		msg_len = 0;
+		break;
+	}
+
+	*response_datalen = 2 * sizeof(rpmi_uint32_t);
+	rsp[0] = rpmi_to_xe32(xport->is_be, (rpmi_int32_t)status);
+	rsp[1] = rpmi_to_xe32(xport->is_be, msg_len);
+
+	DPRINTF("response length = %d status = %ld", rsp[1], status);
+
+	return status;
 }
 
 /* Keep entry index same as service_id value */
