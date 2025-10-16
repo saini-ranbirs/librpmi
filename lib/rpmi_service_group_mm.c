dd79df6d1b38d0c4f9ab95b3aeee7771d1a4f454
Ranbir Singh <rsingh@ventanamicro.com>
2025-10-16 13:11:13 +0530

    lib: Add EFI_MM_READY_TO_BOOT_GUID dummy support
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Signed-off-by: Ranbir Singh <rsingh@ventanamicro.com>

--- lib/rpmi_service_group_mm.c
+++ lib/rpmi_service_group_mm.c
@@ -55,6 +55,7 @@ struct rpmi_mm_comm_header_guid mm_comm_hdr_guid_lut[] = {
 	[1] { EFI_MM_VAR_PROTOCOL_GUID, EFI_MM_VAR_PROTOCOL_GUID_DATA },
 	[2] { EFI_MM_VAR_POLICY_GUID, EFI_MM_VAR_POLICY_GUID_DATA },
 	[3] { EFI_MM_END_OF_DXE_GUID, EFI_MM_END_OF_DXE_GUID_DATA },
+	[4] { EFI_MM_READY_TO_BOOT_GUID, EFI_MM_READY_TO_BOOT_GUID_DATA },
 };
 
 rpmi_uint8_t msg_buffer[17 * 1024];
@@ -70,6 +71,7 @@ static const char *get_hdr_guid_string(enum efi_mm_header_guid guid)
 		STRING_CASE(EFI_MM_VAR_PROTOCOL_GUID);
 		STRING_CASE(EFI_MM_VAR_POLICY_GUID);
 		STRING_CASE(EFI_MM_END_OF_DXE_GUID);
+		STRING_CASE(EFI_MM_READY_TO_BOOT_GUID);
 
 	default:
 		STRING_CASE(EFI_MM_HDR_GUID_UNSUPPORTED);
@@ -379,6 +381,7 @@ static enum rpmi_error rpmi_mm_communicate(struct rpmi_service_group *group,
 		break;
 
 	case EFI_MM_END_OF_DXE_GUID:
+	case EFI_MM_READY_TO_BOOT_GUID:
 		DPRINTF("Handling (dummy) header %s",
 			get_hdr_guid_string(mm_comm_hdr_guid_lut[index].name));
 		status = RPMI_SUCCESS;
